77049c33ae58932de41cef33124cb4503dad0fc5
sajo000 <sajo@deleo.co.kr>
2025-09-15 12:29:58 +0900

    버튼 파운데이션 세팅, 공통 레이아웃과 iframe 레이아웃별로 스타일 분리중

---
+++ non-bootstrap/gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+
+// SCSS → CSS 변환 함수
+function buildLayout() {
+  return gulp.src('sass/app.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(sourcemaps.write('.'))
+  .pipe(gulp.dest('css'));
+}
+
+function buildIframePages() {
+  return gulp.src('sass/pages/iframe-pages.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(sourcemaps.write('.'))
+  .pipe(gulp.dest('css'));
+}
+
+// 전체 SCSS 빌드(동시에 실행)
+const buildAll = gulp.parallel(buildLayout, buildIframePages);
+
+// watch 태스크, 두 파일 모두 감시
+function watch() {
+  gulp.watch('sass/**/*.scss', { ignoreInitial: false }, buildAll);
+}
+
+gulp.task('build', buildAll);     // npm run build 로 전체 빌드
+gulp.task('watch', watch);        // npm run watch 로 감시 시작
+gulp.task('default', watch);      // gulp 만 써도 watch
